39cda151c1e06465a47a275bdb611df49098ec1e
brunamuricy <168845137+brunamuricy@users.noreply.github.com>
2024-11-05 17:49:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é o valor de π (pi) aproximadamente?',
+    'O valor de π é aproximadamente 3,14.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o planeta mais próximo do Sol?',
+    'O planeta mais próximo do Sol é Mercúrio.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a Revolução Francesa?',
+    'A Revolução Francesa aconteceu em 1789.'
+)
